9800343c0b98d963e7917ccf1a14024798403106
Ronja Quensel <ronja.quensel@isst.fraunhofer.de>
2026-05-13 16:04:40 +0200

    chore: license headers

--- src/main/resources/sql/data_flow_schema.sql
+++ src/main/resources/sql/data_flow_schema.sql
@@ -1,3 +1,18 @@
+--
+--  Copyright (c) 2026 Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
+--
+--  This program and the accompanying materials are made available under the
+--  terms of the Apache License, Version 2.0 which is available at
+--  https://www.apache.org/licenses/LICENSE-2.0
+--
+--  SPDX-License-Identifier: Apache-2.0
+--
+--  Contributors:
+--       Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V. - initial script
+--
+
+-- THIS SCHEMA HAS BEEN WRITTEN AND TESTED ONLY FOR POSTGRES
+
 CREATE TABLE IF NOT EXISTS data_flows
 (
     id                      VARCHAR     PRIMARY KEY,
