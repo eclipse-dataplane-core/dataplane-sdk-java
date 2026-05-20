c9d18329496c8d5e78ddc30a7dfa4a1efc0c5e5f
Ronja Quensel <ronja.quensel@isst.fraunhofer.de>
2026-05-20 13:24:00 +0200

    chore: add builder methods for stores on Dataplane

--- src/main/java/org/eclipse/dataplane/Dataplane.java
+++ src/main/java/org/eclipse/dataplane/Dataplane.java
@@ -69,8 +69,8 @@ import static java.util.Collections.emptyMap;
 public class Dataplane {
 
     private final ObjectMapper objectMapper = new ObjectMapper().configure(FAIL_ON_UNKNOWN_PROPERTIES, false);
-    private final DataFlowStore dataFlowStore = new InMemoryDataFlowStore(objectMapper);
-    private final ControlPlaneStore controlPlaneStore = new InMemoryControlPlaneStore(objectMapper);
+    private DataFlowStore dataFlowStore = new InMemoryDataFlowStore(objectMapper);
+    private ControlPlaneStore controlPlaneStore = new InMemoryControlPlaneStore(objectMapper);
     private String id;
     private URI endpoint;
     private final Set<String> transferTypes = new HashSet<>();
@@ -451,6 +451,16 @@ public class Dataplane {
 
         public Builder label(String label) {
             dataplane.labels.add(label);
+            return this;
+        }
+
+        public Builder dataFlowStore(DataFlowStore store) {
+            dataplane.dataFlowStore = store;
+            return this;
+        }
+
+        public Builder controlPlaneStore(ControlPlaneStore store) {
+            dataplane.controlPlaneStore = store;
             return this;
         }
 
