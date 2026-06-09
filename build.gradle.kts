1bab828034a9e84dd2b645a3db86960048059f67
Ronja Quensel <ronja.quensel@isst.fraunhofer.de>
2026-06-09 14:10:33 +0200

    chore: move group and version to properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,9 +8,6 @@ plugins {
 }
 
 subprojects {
-    group = "org.eclipse.dataplane-core"
-    version = "0.0.12-SNAPSHOT"
-
     apply(plugin = "signing")
     apply(plugin = "maven-publish")
     apply(plugin = "com.vanniktech.maven.publish")
